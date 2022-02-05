7d4041a35582314adc78f475211ee164fc0571ab
aswinraj <aswinrajkorkandy@gmail.com>
2022-02-05 14:03:28 +0530

    emp.id

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView,FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const userData = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496"
+      }
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets"
+    }
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618"
+      }
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains"
+    }
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653"
+      }
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications"
+    }
+  },
+ 
+];
+const App = () =>{
+
+  
+  const renderItem = ({ item }) => ( 
+    <View
+     style={styles.item}>
+
+    <View style={styles.item}>
+    <Text style={styles.title}>{item.name}</Text>
+   <Text style ={styles.sub_title}>{item.address.street}</Text>
+   <Text style ={styles.sub_title}>{item.address.suite+","+item.city}</Text>
+    <Text style ={styles.comp}>{item.company.name}</Text>
+    <Text style ={styles.comp}>{item.company.catchPhrase+","+item.bs}</Text>
+    
     </View>
+    
+  </View>
+  );
+
+
+  return (
+  
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={userData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 50,
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 5,
+    flexDirection:'column',
+    marginVertical: 10,
+    marginHorizontal: 5,
+  },
+  title: {
+    fontSize: 22,
+  },
+  
 });
+
+export default App;
